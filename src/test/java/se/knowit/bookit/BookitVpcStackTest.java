a16d2385d941568b3cd70de55fc18ff082d5eeb8
ola.mattsson <ola.mattsson@gmail.com>
2020-02-20 13:16:14 +0100

    Changed package and class names

--- src/test/java/se/knowit/bookit/BookitVpcStackTest.java
+++ src/test/java/se/knowit/bookit/BookitVpcStackTest.java
@@ -1,23 +1,23 @@
-package com.myorg;
+package se.knowit.bookit;
 
-import software.amazon.awscdk.core.App;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import org.junit.Test;
+import software.amazon.awscdk.core.App;
 
 import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
 
-public class BookitAwscdkTest {
+public class BookitVpcStackTest {
     private final static ObjectMapper JSON =
         new ObjectMapper().configure(SerializationFeature.INDENT_OUTPUT, true);
 
     @Test
     public void testStack() throws IOException {
         App app = new App();
-        BookitAwscdkStack stack = new BookitAwscdkStack(app, "test");
+        VpcStack stack = new VpcStack(app, "test");
 
         // synthesize the stack to a CloudFormation template and compare against
         // a checked-in JSON file.
