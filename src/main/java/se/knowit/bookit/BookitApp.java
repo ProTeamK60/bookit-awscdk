90f298281f8a25d1f128e962350481c47451f4ac
ola.mattsson <ola.mattsson@gmail.com>
2020-02-24 18:11:36 +0100

    Config for App and Common stack

--- src/main/java/se/knowit/bookit/BookitApp.java
+++ src/main/java/se/knowit/bookit/BookitApp.java
@@ -1,13 +1,34 @@
 package se.knowit.bookit;
 
+import org.aeonbits.owner.Config;
+import org.aeonbits.owner.ConfigFactory;
 import software.amazon.awscdk.core.App;
+import software.amazon.awscdk.core.Environment;
+import software.amazon.awscdk.core.StackProps;
 
 public class BookitApp {
+    @Config.LoadPolicy(Config.LoadType.MERGE)
+    /*
+    * system property overrides environment property which overrides
+    * se/knowit/bookit/BookitApp$BookitAppConfig.properties
+    * */
+    @Config.Sources({"system:properties", "system:env"})
+    private interface BookitAppConfig extends Config {
+        @Key("aws.accountId")
+        String awsAccountId();
+        
+        @Key("aws.region")
+        String awsRegion();
+    }
     public static void main(final String[] args) {
         App app = new App();
-
-        new BookitCommonStack(app, "BookitCommonStack");
-
+        BookitAppConfig config = ConfigFactory.create(BookitAppConfig.class);
+        StackProps stackProps = StackProps.builder().env(
+                Environment.builder()
+                        .account(config.awsAccountId())
+                        .region(config.awsRegion())
+                        .build()).build();
+        BookitCommonStack bookitCommonStack = new BookitCommonStack(app, "BookitCommonStack", stackProps);
         app.synth();
     }
 }
--- src/main/java/se/knowit/bookit/BookitCommonStack.java
+++ src/main/java/se/knowit/bookit/BookitCommonStack.java
@@ -1,5 +1,7 @@
 package se.knowit.bookit;
 
+import org.aeonbits.owner.Config;
+import org.aeonbits.owner.ConfigFactory;
 import software.amazon.awscdk.core.Construct;
 import software.amazon.awscdk.core.RemovalPolicy;
 import software.amazon.awscdk.core.Stack;
@@ -10,7 +12,22 @@ import software.amazon.awscdk.services.logs.RetentionDays;
 import software.amazon.awscdk.services.servicediscovery.PrivateDnsNamespace;
 
 public class BookitCommonStack extends Stack {
-    
+    private interface BookitCommonStackConfig extends Config {
+        @Key("vpc.maxAzs")
+        @DefaultValue("3")
+        int maxAzs();
+        
+        String namespaceName();
+        
+        @DefaultValue("bookit-test-logs")
+        String logGroupName();
+        
+        @DefaultValue("DESTROY")
+        RemovalPolicy logRemovalPolicy();
+        
+        @DefaultValue("TWO_WEEKS")
+        RetentionDays logRetentionTime();
+    }
     private final Vpc vpc;
     private final PrivateDnsNamespace dnsNamespace;
     private final LogGroup logGroup;
@@ -21,21 +38,22 @@ public class BookitCommonStack extends Stack {
 
     public BookitCommonStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
-
+        BookitCommonStackConfig config = ConfigFactory.create(BookitCommonStackConfig.class);
+        System.out.println("config = " + config);
         // The code that defines your stack goes here
         vpc = Vpc.Builder.create(this, "vpc")
-                .maxAzs(3)  // Default is all AZs in region
+                .maxAzs(config.maxAzs())  // Default is all AZs in region
                 .build();
         dnsNamespace = PrivateDnsNamespace.Builder
                 .create(this, "dnsNamespace")
                 .vpc(vpc)
-                .name("bookit")
+                .name(config.namespaceName())
                 .build();
         logGroup = LogGroup.Builder
                 .create(this, "logGroup")
-                .logGroupName("bookit-logs")
-                .removalPolicy(RemovalPolicy.DESTROY)
-                .retention(RetentionDays.TWO_WEEKS)
+                .logGroupName(config.logGroupName())
+                .removalPolicy(config.logRemovalPolicy())
+                .retention(config.logRetentionTime())
                 .build();
     }
     
